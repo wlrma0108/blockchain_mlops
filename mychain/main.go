fd590d9449025c6767d76e11186bb85a401d0ac1
wlrma0108 <dlwlrma0108@gmail.com>
2025-05-26 23:12:13 +0900

    go

---
+++ mychain/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Latest block number:", header.Number)
+}
