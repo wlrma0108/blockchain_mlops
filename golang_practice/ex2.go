a80dbb5ac6c08a482079b82523229fe2209dca50
wlrma0108 <dlwlrma0108@gmail.com>
2025-07-11 22:28:20 +0900

    commit stack coding little bit time

--- golang_practice/ex2.go
+++ golang_practice/ex2.go
@@ -41,12 +41,24 @@ func (l *LinkedList) print() {
 	}
 	fmt.Println("nil")
 }
-
 type Stack struct {
-	data []int
+    data []int
 }
 
 func (s *Stack) Push(v int) {
-	s.data = append(s.data, v)
+    s.data = append(s.data, v)
+}
+
+func (s *Stack) Pop() (int, bool) {
+    if len(s.data) == 0 {
+        return 0, false
+    }
+    v := s.data[len(s.data)-1]
+    s.data = s.data[:len(s.data)-1]
+    return v, true
+}
+
+func (s *Stack) IsEmpty() bool {
+    return len(s.data) == 0
 }
 
